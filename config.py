fbd2e3e085d2a4921bbc2e9b33a4d483634f05a4
Yidi-Wang <wang5615@umn.edu>
2016-07-11 00:28:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 #config twitter API credential here
 #config twitter API credential here
-consumer_key = '5i69gbXcIRBouNygjEO4CmwnE'
-consumer_secret = 'ThkVHwXA5XVxEbMYbn8sjO5HXZ3A6smrcB176s0KeCCgikTCyw'
-oauth_token = '2789068470-vuqIRvDTfDWnjUcGv0arMC608uUbqlMO60DycF7'
-oauth_token_secret = 'jGBdJ5SSQWsL2YVbTyiieguSkcPq2290ulyN2mQqKfWgy'
+consumer_key = ''
+consumer_secret = ''
+oauth_token = ''
+oauth_token_secret = ''
